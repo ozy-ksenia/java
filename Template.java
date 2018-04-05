44348c2c2c68025ff0b6794fdd85fd90c0d26914
ozy-ksenia <32943579+ozy-ksenia@users.noreply.github.com>
2018-04-05 09:05:59 -0700

    Add files via upload

---
+++ Template.java
@@ -0,0 +1,42 @@
+package Template;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Template {
+	private WebDriver driver;
+	private String baseUrl;
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "geckodriver");
+		driver = new FirefoxDriver();
+		baseUrl = "http://hrm.seleniumminutes.com";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// tearDown
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	private void sleep(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
